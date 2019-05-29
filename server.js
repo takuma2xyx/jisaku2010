2fa508937a829fbe6f7d8919c95e0b4d625bf99e
takuma2xyx <50941426+takuma2xyx@users.noreply.github.com>
2019-05-29 16:22:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 3000;
 
 const config = {
-    channelSecret: 'baf64668049efd5f9a1e79b1e7ddbe34',
-    channelAccessToken: 'KQYjdJjTX6qhegZD+yJzQoU5cAHr8rbRPqUO9/bdfKfnwKkczNIsHThDxjnPl1XYvyRqRd06NcXE03c2mvdOmYC2Bws1Bc3IdrZH8esw7qsfFHWI9eJs8dHA/pDpQO1iLFPENUTWCCOYHqUVQvE9tQdB04t89/1O/w1cDnyilFU='
+    channelSecret: '1622e06d8c6c7f429c00e98ecd26ff4a',
+    channelAccessToken: '4ZdpR9ani2nxy4ERRvMGQA7RYuiejjYrnTrDF4430ZWLL4KTPEI9W2hAIHbVf0CYs/n3TU7oJ/TMQwsiEytEvVEvuDCYBPlpDMbZwMkJngLmexxtPS4Tq0AWkwIgjELdGF0mRRkTm6N/ErN+Iyd96wdB04t89/1O/w1cDnyilFU='
 };
 
 const app = express();
